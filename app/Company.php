8d81a285a67ee78f64b4b3162d7c8a2db480a59a
Joao Ricardo Lima <j.ricardo_lima@outlook.com>
2020-05-05 18:36:25 -0400

    Criado o relacionamento com contas

--- app/Company.php
+++ app/Company.php
@@ -26,7 +26,10 @@ class Company extends Model
         'email',
     ];
 
-
+    public function accountRelation()
+    {
+        return $this->hasMany(Account::class,'company_id','id');
+    }
     public function setPhysicPersonAttribute($value)
     {
         return $this->attributes['physic_person'] = ($value == "1" ? 1 : 0);
