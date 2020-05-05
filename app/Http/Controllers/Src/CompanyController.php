ae86b5a59ea5484a9b53d0f243bf436c87cb3c9e
Joao Ricardo Lima <j.ricardo_lima@outlook.com>
2020-05-05 10:42:41 -0400

    Implementado os métodos search,store e edit

--- app/Http/Controllers/Src/CompanyController.php
+++ app/Http/Controllers/Src/CompanyController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers\Src;
 
+use App\Company;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Src\CompanyRequest;
+use App\Utils\DocumentsValidator;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
@@ -27,15 +30,62 @@ class CompanyController extends Controller
         return view('admin.administrativo.fornecedores.create');
     }
 
+    public function search()
+    {
+        $companies = Company::all();
+
+        return view('admin.administrativo.fornecedores.search')->with([
+            'companies' => $companies
+        ]);
+    }
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CompanyRequest $request)
     {
-        //
+        $company = new Company();
+        $validateDocuments = null;
+
+        if($request->physic_person){
+            $validateDocuments = (new DocumentsValidator())->cpfValid($request->document_primary);
+            if($validateDocuments){
+                $company->document_primary = $request->document_primary;
+                $company->physic_person = 1;
+            }else{
+                session()->flash('messageInfo','Warning@CPF é invalido');
+                return back()->withInput();
+            }
+        } else {
+           $validateDocuments = (new DocumentsValidator())->cnpjValid($request->document_company_identification);
+
+           if($validateDocuments){
+               $company->document_company_identification = $request->document_company_identification;
+           } else {
+               session()->flash('messageInfo','Warning@CNPJ é invalido');
+               return back()->withInput();
+           }
+        }
+
+        $company->fantasy_name = $request->fantasy_name;
+        $company->company_name = $request->company_name;
+        $company->zipcode = $request->zipcode;
+        $company->state = $request->state;
+        $company->city = $request->city;
+        $company->street = $request->street;
+        $company->neighborhood = $request->neighborhood;
+        $company->contact_one = $request->contact_one;
+        $company->contact_two = $request->contact_two;
+        $company->email = $request->email;
+
+        $company->save();
+        session()->flash('messageInfo','Success@Empresa cadastrada com sucesso');
+        return redirect()->route('source.companies.create');
+
+
+
     }
 
     /**
@@ -57,7 +107,8 @@ class CompanyController extends Controller
      */
     public function edit($id)
     {
-        //
+        $company = Company::find($id);
+        return view('admin.administrativo.fornecedores.edit')->with(['company' => $company]);
     }
 
     /**
@@ -69,7 +120,7 @@ class CompanyController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+
     }
 
     /**
