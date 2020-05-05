293dc90be43eeb330b43cc43c1a4a5557c650bf0
Joao Ricardo Lima <j.ricardo_lima@outlook.com>
2020-05-05 10:45:59 -0400

    Implementado o método para manipular os campos cpf e cnpj do fornecedor

--- public/front/assets/scripts/GeneralFunction.js
+++ public/front/assets/scripts/GeneralFunction.js
@@ -23,13 +23,23 @@ export class GeneralFunction{
                 })
     }
 
-    hasValue(id){
-        let value = document.getElementById(id).value;
-
-        if(value){
-            return true;
-        }else{
-            return false;
+    cpfCnpjField(id,cnpjId,cpfId){
+        if(id){
+            $("#"+cpfId).attr('disabled','disabled');
+            $("#"+cnpjId).attr('disabled','disabled');
+            $("#"+id).change(function(){
+                let value = $(this).val();
+                if(value == 0){
+                    $("#"+cpfId).attr('disabled','disabled');
+                    $("#"+cnpjId).removeAttr('disabled');
+                } else if(value == 1){
+                    $("#"+cnpjId).attr('disabled','disabled');
+                    $("#"+cpfId).removeAttr('disabled');
+                } else {
+                    $("#"+cnpjId).attr('disabled','disabled');
+                    $("#"+cpfId).attr('disabled','disabled');
+                }
+            });
         }
     }
 }
